73e65d51a448eb7cb9bccb5f38f0782277ea322f
Rafael Chacon <rafaelchacon@gmail.com>
2012-09-13 14:24:25 -0700

    Add sprintly-org first implementation

---
+++ sprintly-org.el
@@ -0,0 +1,54 @@
+(require 'json)
+(defvar sprintly-user-name "")
+(defvar sprintly-token "")
+
+(defvar sprintly-block-authorisation nil
+  "Flag whether to block url.el's usual interactive authorisation procedure")
+
+(defadvice url-http-handle-authentication (around xyz-fix)
+  (unless sprintly-block-authorisation
+    ad-do-it))
+(ad-activate 'url-http-handle-authentication)
+
+(defun get-json (url)
+  (let ((buffer (url-retrieve-synchronously url))
+        (json nil))
+    (save-excursion
+      (set-buffer buffer)
+      (goto-char (point-min))
+      (re-search-forward "^$" nil 'move)
+      (setq json (buffer-substring-no-properties (point) (point-max)))
+      (kill-buffer (current-buffer)))
+    json))
+
+(defun get-and-parse-json (url)
+    (let ((json-object-type 'plist))
+          (json-read-from-string
+                (get-json url))))
+
+(defun retrieve-elem-info (plem)
+  (let ((number (plist-get plem :number))
+        (type (plist-get plem :type))
+        (title (plist-get plem :title)))
+        (insert (format "* TODO %s #%d\n  Description: %s\n" type number title))))
+
+(defun get-number-titles (url)
+  (let ((json (get-and-parse-json url)))
+        (mapcar 'retrieve-elem-info json)))
+
+(defun insert-sprintly-active-items ()
+  (interactive)
+  (let ((sprintly-block-authorisation t)
+        (url-request-method "GET")
+        (url-request-extra-headers
+         `(("Content-Type" . "application/xml")
+           ("Authorization" . ,(concat "Basic "
+                                       (base64-encode-string
+                                        (concat sprintly-user-name ":" sprintly-token)))))))
+    (get-number-titles "https://sprint.ly/api/products/5139/items.json?assigned_to=6280&status=in-progress")))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (local-set-key "\M-n"
+                           'insert-sprintly-active-items)))
+(provide 'sprintly-org)
